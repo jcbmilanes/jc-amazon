de24aa8fa6f66523c294f0c15274897067e11a86
JCM <johncm2k8g@gmail.com>
2020-10-10 22:32:00 +0800

    Subtotal price computation

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -2,13 +2,13 @@ import React from 'react';
 import './Subtotal.css'
 import CurrencyFormat from 'react-currency-format';
 import { useStateValue } from './StateProvider';
+import { getBasketTotal } from './reducer';
 
 function Subtotal() {
     const [{basket}, dispatch] = useStateValue();
 
     return (
         <div className="subtotal">
-
             <CurrencyFormat
                 renderText={(value) => (
                     <>
@@ -23,7 +23,7 @@ function Subtotal() {
                 )}
   
                 decimalScale={2}
-                value={0}
+                value={getBasketTotal(basket)}
                 displayType={"text"}
                 thousandSeparator={true}
                 prefix={"$"}
